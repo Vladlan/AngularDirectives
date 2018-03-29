2395f62739c3c221daeef482b17bc85a7931df79
Vladlan <lanvlad911@gmail.com>
2018-03-29 20:07:39 +0300

    finished the fifth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
   headerText = 'Creating directives';
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+
+  onClick(num: number) {
+    this.current = num;
+  }
 }
